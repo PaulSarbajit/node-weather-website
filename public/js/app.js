e83f7bccfb7335fb7b8a2516d90a57a339a8789e
Sarbajit Paul <sarbajit.paul@edgistify.com>
2020-11-16 21:04:57 +0530

    Removed extra console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("Web server is also connected with javascript");
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 messageOne = document.querySelector('#message-1');
